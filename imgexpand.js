8c76b04f4b6619a6bcd4e785e59b2937a2b25339
mikeyvalentine <michaelsmailmachine@gmail.com>
2022-10-13 16:54:19 -0700

    asdf

--- imgexpand.js
+++ imgexpand.js
@@ -1,24 +1,29 @@
 const img = document.getElementsByTagName('img');
-Array.from(img).forEach(element => 
-    element.onclick = function(){
-        if (element.id == 'flowernospin'){
-        } else {
-            if (element.classList.contains('expanded')){
-                if (element.id == 'landscape'){
-                    element.style.width = '60vw';
-                } else {
-                    element.style.height = '80vh';
-                }
-                element.classList.remove('expanded');
-            } else {
-                if (element.id == 'landscape'){
-                    element.style.width = '80vw';
-                } else {
-                    element.style.height = '120vh';
-                }
-                element.classList.add('expanded');
-                console.log(1);
-            }
-        }
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
-    );
+    if (!isMobile()) {
+        Array.from(img).forEach(element => 
+            element.onclick = function(){
+                if (element.id == 'flowernospin'){
+                } else {
+                    if (element.classList.contains('expanded')){
+                        if (element.id == 'landscape'){
+                            element.style.width = '60vw';
+                        } else {
+                            element.style.height = '80vh';
+                        }
+                        element.classList.remove('expanded');
+                    } else {
+                        if (element.id == 'landscape'){
+                            element.style.width = '80vw';
+                        } else {
+                            element.style.height = '120vh';
+                        }
+                        element.classList.add('expanded');
+                        console.log(1);
+                    }
+                }
+            }
+            );
+        }
